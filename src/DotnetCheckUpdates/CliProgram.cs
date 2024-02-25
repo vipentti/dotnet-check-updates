a6bdceeff77bfd11dc28629eeaf52c1f3a5fa8c0
Ville Penttinen <villem.penttinen@gmail.com>
2024-02-25 13:24:48 +0000

    Fix CliProgram character encoding

--- src/DotnetCheckUpdates/CliProgram.cs
+++ src/DotnetCheckUpdates/CliProgram.cs
@@ -1,4 +1,4 @@
-// Copyright 2023-2024 Ville Penttinen
+﻿// Copyright 2023-2024 Ville Penttinen
 // Distributed under the MIT License.
 // https://github.com/vipentti/dotnet-check-updates/blob/main/LICENSE.md
 
